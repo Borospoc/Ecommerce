d44a41a052806b03c2f2492f46a9e242c198a41b
Borospoc <94739041+Borospoc@users.noreply.github.com>
2022-04-15 19:28:08 +0700

    Update views.py

--- store/views.py
+++ store/views.py
@@ -222,3 +222,9 @@ def viewOrder(request,order_id):
 
 def thankyou(request):
     return render(request,'thankyou.html')
+
+def home(request):
+    message='id:%s, username:%s firstname:%s, lastname:%s'%(request.user.pk,request.user.username,request.user.first_name,request.user.last_name)
+    now = datetime.datetime.now()
+    html = '<html><body><p>It is now %s,</p><p>%s</p></body></html>' %(now,message)
+    return HttpResponse(html)
